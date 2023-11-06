083297e97169ae19f3a8a3b6e3c41d54ff3aed54
BellaTr <boi.tran@edu.sait.ca>
2023-11-06 16:18:39 -0700

    week8 updates

---
+++ app/week8/shopping-list/page.js
@@ -0,0 +1,39 @@
+"use client";
+import { useState } from 'react';
+import Link from 'next/link';
+import ItemList from './item-list.js';
+import NewItem from './new-item';
+import MealIdeas from './meal-ideas.js';
+import itemsData from './items.json';
+import { useUserAuth } from '../_utils/auth-context.js';    
+
+export default function page() {
+    const {user} = useUserAuth();
+
+    if (!user) {
+        return null;
+    }
+
+    const [items, setItems] = useState(itemsData);
+    const [selectedItemName, setSelectedItemName] = useState('');
+
+    const handleAddItem = (newItem) => {
+        setItems([...items, newItem]);
+    
+    };
+    const handleItemSelect = (item) => {
+        const cleanItemName = selectedItem.name.split(',')[0].trim();
+        setSelectedItemName(cleanItemName);
+    };
+
+    return (
+        <main className="bg-indigo-600" style={{display: 'flex', justifyContent: 'space-around'}}>
+            <div>
+                <h1 className="text-lg">Shopping List</h1>
+                <NewItem onAddItem={handleAddItem} />
+                <ItemList items={items} onItemSelect={handleItemSelect} />
+            </div>
+            <div>{selectedItemNae && <MealIdeas ingredient={selectedItemName} />}</div>
+        </main>
+    );
+}
